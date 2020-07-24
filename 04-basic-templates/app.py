229a868eec68e3c8f9328102493e725970fed7f6
Kun <extrakun@yahoo.com.sg>
2020-07-24 05:52:23 +0000

    Initial commit

---
+++ 04-basic-templates/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    pass
+
+@app.route('/gallery')
+def gallery():
+    pass
+
+# "magic code" -- boilerplate
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
